7d4b267621c64ac114984b42da86184ab5e7ec0f
Radina <56393689+radinan@users.noreply.github.com>
2021-01-11 18:04:17 +0200

    default constructor and desructor + AVL all_songs
    further work expected here! (without def constructors couldn't test playlist funcs)

--- project/Library.cpp
+++ project/Library.cpp
@@ -2,11 +2,15 @@
 #include <iostream>
 #include <string>
 
-User Library::get_user() const
+Library::Library() {}
+
+Library::~Library() {}
+
+User& Library::get_user() 
 {
 	return curr_user;
 }
-Playlist Library::get_playlist() const
+Playlist& Library::get_playlist() 
 {
 	return curr_playlist;
 }
@@ -20,9 +24,9 @@ void Library::add_playlist(const Playlist& other)
 {
 	curr_playlist = other;
 }
-
+/*
 void Library::add_song(const Song& other)
 {
 	//all_songs.insert(other);
 }
-
+*/
--- project/Library.h
+++ project/Library.h
@@ -8,19 +8,22 @@
 //contains elements that are being used or needed
 class Library 
 {
-private:
+private: //!!!think about copies and whether you want to change the original user or not!!!
 	User curr_user; //recently logged; current
-	AVLTree<Song> all_songs; //in the whole library
+	//AVLTree all_songs; //in the whole library
 	Playlist curr_playlist;
 
 public:
 	//big 4
-	User get_user() const;
-	Playlist get_playlist() const;
+	Library();
+	~Library();
+
+	User& get_user();
+	Playlist& get_playlist();
 
 	void add_user(const User& other);
 	void add_playlist(const Playlist& other);
-	void add_song(const Song& other);
+	//void add_song(const Song& other);
 
 };
 
