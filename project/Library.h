281cfe48b5e544a9fc4b90c9c0fc66460dbb9765
Radina <56393689+radinan@users.noreply.github.com>
2021-01-12 21:31:58 +0200

    added default constructor

--- project/Library.h
+++ project/Library.h
@@ -8,21 +8,23 @@
 //contains elements that are being used or needed
 class Library 
 {
-private: //!!!think about copies and whether you want to change the original user or not!!!
+private: //!!!think about copies and whether you want to change the original user or not!!! wtf?
 	User curr_user; //recently logged; current
 	Playlist curr_playlist;
-	AVLTree all_songs; //in the whole library //the original avl tree with all songs is always sorted by name!
+	AVLTree all_songs; //in the whole library 
+					   //the original avl tree with all songs is always sorted by name!
 public:
-	Library(const AVLTree& other);
+	//Library(const AVLTree& other);
+	Library();
 	~Library();
 
 	User& get_user();
 	Playlist& get_playlist();
 	AVLTree& get_songs();
 
-	void add_user(const User& other);
-	void add_playlist(const Playlist& other);
-	void add_song(const Song& other);
+	void set_user(const User& other); //loads user
+	void set_playlist(const Playlist& other); //loads playlist
+	void add_song(const Song& other); //adds song to the tree
 
 
 };
