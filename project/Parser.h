3259cf35dee6e50df11852a7eea0712b0466351b
Radina <56393689+radinan@users.noreply.github.com>
2021-01-12 16:36:28 +0200

    re-arranged funcs

--- project/Parser.h
+++ project/Parser.h
@@ -1,84 +1,13 @@
 #pragma once
 //#include "AVLTree.h"
 #include "Library.h"
+#include <string>
 #include <queue>
 #include <sstream>
+//:)))))))))))))
+//change enum to enum class
+//re-read the whole code!
 
-class Expression //WHITE SPACES!!!!!
-{
-private:
-    std::string input; //user's input
-    std::queue<Statement> q_st; //all statements (separated by ||)
-private:
-    void parse_expression() //adds to queue
-    {
-        char delimeter('|');
-        std::stringstream ss(input);
-        while (std::getline(ss, input, delimeter)) //reads and splits: statement || statement...
-        {
-            std::string str = input; //inache dava problem
-            Statement st(str);
-            q_st.push(st); //pushing it parsed
-        }
-    }
-public:
-    Expression(const std::string& _input) 
-        : input(_input) 
-    {
-        parse_expression();
-    }
-
-    AVLTree& do_expression(Library& lib)
-    {
-        while (!q_st.empty()) //all elements
-        {
-            AVLTree pl_tree = q_st.front().do_statement(lib); //playlist tree
-            if (pl_tree.is_empty()) //if no tree for the criteria => empty
-                q_st.pop(); //check next statement
-            else
-                return pl_tree; //return the playlist
-        }
-        //return empty?
-    }
-};
-class Statement
-{
-private:
-    std::string st;
-    std::queue<Command> q_co; //all commands (separated by &&)
-private:
-    void parse_statement() //adds to queue
-    {
-        char delimeter('&');
-        std::stringstream ss(st);
-        while (std::getline(ss, st, delimeter)) //reads and splits: command && command...
-        {
-            std::string str = ss.str();//inache dava problem
-            Command co(str);
-            q_co.push(co); //pushing it parsed
-        }
-    }
-public:
-    Statement(const std::string& _st) 
-        : st(_st) 
-    {
-        parse_statement();
-    }
-
-    AVLTree& do_statement(Library& lib) //bool node* or tree&
-    {
-        AVLTree original(lib.get_songs());
-
-        while (!q_co.empty()) //all elements
-        {
-            original = q_co.front().do_command(lib, original); //change the playlist tree
-            if (original.is_empty()) //doesn't find matching songs
-                break;
-            q_co.pop(); //get next command
-        }
-        return original; //returns either empty or playlist tree
-    }
-};
 class Command
 {
 private:
@@ -151,8 +80,8 @@ private:
         opt = three;
     }
 public:
-    Command(const std::string& _co) 
-        : co(_co) 
+    Command(const std::string& _co)
+        : co(_co)
     {
         parse_command();
     }
@@ -241,3 +170,81 @@ public:
         }
     }
 };
+class Statement
+{
+private:
+    std::string st;
+    std::queue<Command> q_co; //all commands (separated by &&)
+private:
+    void parse_statement() //adds to queue
+    {
+        char delimeter('&');
+        std::stringstream ss(st);
+        while (std::getline(ss, st, delimeter)) //reads and splits: command && command...
+        {
+            std::string str = ss.str();//inache dava problem
+            Command co(str);
+            q_co.push(co); //pushing it parsed
+        }
+    }
+public:
+    Statement() {}
+    Statement(const std::string& _st)
+        : st(_st)
+    {
+        parse_statement();
+    }
+    ~Statement() {}
+    AVLTree& do_statement(Library& lib) //bool node* or tree&
+    {
+        AVLTree original(lib.get_songs());
+
+        while (!q_co.empty()) //all elements
+        {
+            original = q_co.front().do_command(lib, original); //change the playlist tree
+            if (original.is_empty()) //doesn't find matching songs
+                break;
+            q_co.pop(); //get next command
+        }
+        return original; //returns either empty or playlist tree
+    }
+};
+class Expression //WHITE SPACES!!!!!
+{
+private:
+    std::string input; //user's input
+    std::queue<Statement> q_st; //all statements (separated by ||)
+private:
+    void parse_expression() //adds to queue
+    {
+        char delimeter('|');
+        std::stringstream ss(input);
+        while (std::getline(ss, input, delimeter)) //reads and splits: statement || statement...
+        {
+            Statement st(input);
+            q_st.push(st); //pushing it parsed
+        }
+    }
+public:
+    Expression() {}
+    Expression(const std::string& _input) 
+        : input(_input) 
+    {
+        parse_expression();
+    }
+    ~Expression() {}
+
+    AVLTree& do_expression(Library& lib)
+    {
+        while (!q_st.empty()) //all elements
+        {
+            AVLTree pl_tree = q_st.front().do_statement(lib); //playlist tree
+            if (pl_tree.is_empty()) //if no tree for the criteria => empty
+                q_st.pop(); //check next statement
+            else
+                return pl_tree; //return the playlist
+        }
+        //return empty?
+    }
+};
+
