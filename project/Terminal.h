1fddf0e534e36682e274e55fe77d1fbbae841631
Radina <56393689+radinan@users.noreply.github.com>
2021-01-22 01:13:13 +0200

    create terminal.h
    so main func can be max clean

---
+++ project/Terminal.h
@@ -0,0 +1,86 @@
+#pragma once
+#include "Library.h"
+
+class Terminal
+{
+public:
+	static void start()
+	{
+		Library lib; //created library
+		int input = -1;
+
+		lib.welcome();
+		while (input != 0)
+		{
+			std::cout << std::endl << "Enter command: ";
+			std::cin >> input;
+			std::cout << std::endl;
+
+			switch (input)
+			{
+			case 0:
+			{
+				lib.exit();
+				break;
+			}
+			case 1:
+			{
+				lib.help();
+				break;
+			}
+			case 2:
+			{
+				lib.sign_in();
+				break;
+			}
+			case 3:
+			{
+				lib.sign_up();
+				break;
+			}
+			case 4:
+			{
+				lib.change_data();
+				break;
+			}
+			case 5:
+			{
+				lib.save_user_data();
+				break;
+			}
+			case 6:
+			{
+				lib.add_song();
+				break;
+			}
+			case 7:
+			{
+				lib.rate_song();
+				break;
+			}
+			case 8:
+			{
+				lib.generate_playlist();
+				break;
+			}
+			case 9:
+			{
+				lib.save_playlist();
+				break;
+			}
+			case 10:
+			{
+				lib.load_playlist();
+				break;
+			}
+			case 11:
+			{
+				lib.show_all_info();
+				break;
+			}
+			}
+		}
+
+	}
+};
+
--- project/main.cpp
+++ project/main.cpp
@@ -1,84 +1,11 @@
 #include <iostream>
-#include <string>
-#include "Library.h"
+#include "Terminal.h"
 
 
 
 int main()
 {
-	Library lib;//created library
-	int input = -1;
-	lib.welcome();
-	while(input != 0)
-	{
-		std::cout << std::endl << "Enter your command: ";
-		std::cin >> input;
-		std::cout << std::endl;
-		switch (input)
-		{
-		case 0:
-		{
-			break; 
-		}
-		case 1:
-		{
-			lib.help();
-			break;
-		}
-		case 2:
-		{
-			lib.sign_in();
-			break;
-		}
-		case 3:
-		{
-			lib.sign_up();
-			break;
-		}
-		case 4:
-		{
-			lib.change_data();
-			break;
-		}
-		case 5:
-		{
-			lib.save_user_data();
-			break;
-		}
-		case 6:
-		{
-			lib.add_song();
-			break;
-		}
-		case 7:
-		{
-			lib.rate_song();
-			break;
-		}
-		case 8:
-		{
-			lib.generate_playlist();
-			break;
-		}
-		case 9:
-		{
-			lib.save_playlist();
-			break;
-		}
-		case 10:
-		{
-			lib.load_playlist();
-			break;
-		}
-		case 11:
-		{
-			lib.show_all_info();
-			break;
-		}
-		}
-	}
+	Terminal::start();
 
-
-	lib.save_songs();
-	std::cout << "END\n";
+	return 0;
 }
